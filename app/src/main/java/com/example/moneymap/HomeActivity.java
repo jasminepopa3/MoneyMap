d44b63f720c8c93eed3077b38d03177443218f83
Antonia <antoniapopeanga37@gmail.com>
2025-03-23 22:23:48 +0200

    Added expenses/budget values in home page

--- app/src/main/java/com/example/moneymap/HomeActivity.java
+++ app/src/main/java/com/example/moneymap/HomeActivity.java
@@ -5,17 +5,35 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    private double totalExpenses = 0.0;
+    private double totalBudget = 0.0;
+
+    private int currentMonthIndex;
+    private int currentYear;
+
+    private ProgressBar loadingBar;
+    private int loadingCounter = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +44,22 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         ToolbarUtils.setupToolbar(this, toolbar);
 
-
+        loadingBar = findViewById(R.id.loadingBar);
         CalendarView calendarView = findViewById(R.id.calendarView);
 
-        // Setează ziua curentă ca fiind selectată
         Calendar calendar = Calendar.getInstance();
-        long currentDate = calendar.getTimeInMillis();
-        calendarView.setDate(currentDate, true, true);
+        currentMonthIndex = calendar.get(Calendar.MONTH);
+        currentYear = calendar.get(Calendar.YEAR);
 
-        // Setează un OnDateChangeListener pentru a bloca selecția zilelor
         calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
-            // Ignoră selecția zilei (nu face nimic)
-            // Re-setăm ziua curentă ca fiind selectată
-            calendarView.setDate(currentDate, true, true);
+            Calendar selectedCalendar = Calendar.getInstance();
+            selectedCalendar.set(year, month, dayOfMonth);
+            if (month != currentMonthIndex || year != currentYear) {
+                currentMonthIndex = month;
+                currentYear = year;
+                fetchTotalExpenses();
+                fetchUserBudget();
+            }
         });
 
         // Găsește butonul "CATEGORII" și setează un OnClickListener
@@ -63,6 +84,139 @@ public class HomeActivity extends AppCompatActivity {
             Intent intent = new Intent(HomeActivity.this, ExpenseActivity.class);
             startActivity(intent);
         });
+
+        // total cheltuieli
+        fetchTotalExpenses();
+        // bugetul
+        fetchUserBudget();
+    }
+
+    private void fetchTotalExpenses() {
+        loadingBar.setVisibility(View.VISIBLE);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String userId = user.getUid();
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            String[] monthsArray = getResources().getStringArray(R.array.months);
+            String currentMonth = monthsArray[currentMonthIndex];
+            String currentYearStr = String.valueOf(currentYear);
+
+            db.collection("users").document(userId).collection("expenses")
+                    .whereEqualTo("month", currentMonth)
+                    .whereEqualTo("year", currentYearStr)
+                    .get()
+                    .addOnSuccessListener(queryDocumentSnapshots -> {
+                        totalExpenses = 0.0;
+
+                        // suma cheltuieli
+                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                            Double sum = document.getDouble("sum");
+                            if (sum != null) {
+                                totalExpenses += sum;
+                            }
+                        }
+
+                        checkLoadingComplete();
+                        updateRatioUI();
+                    })
+                    .addOnFailureListener(e -> {
+                        Log.e("HomeActivity", "Error loading expenses", e);
+                        Toast.makeText(this, "Eroare la încărcarea cheltuielilor", Toast.LENGTH_SHORT).show();
+
+                    });
+        } else {
+            Toast.makeText(this, "User-ul nu este logat", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void fetchUserBudget() {
+        loadingBar.setVisibility(View.VISIBLE);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            String userId = user.getUid();
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            db.collection("users").document(userId).collection("categories")
+                    .get()
+                    .addOnSuccessListener(queryDocumentSnapshots -> {
+                        List<Category> categories = new ArrayList<>(); // Listă locală pentru categorii
+                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                            String id = document.getId();
+                            String name = document.getString("name");
+                            String description = document.getString("description");
+                            String color = document.getString("color");
+
+                            if (name != null && color != null) {
+                                Category category = new Category(id, name, description, color);
+                                categories.add(category);
+                            }
+                        }
+
+                        //bugetele pentru toate categoriile
+                        loadBudgetsForCategories(categories);
+                    })
+                    .addOnFailureListener(e -> {
+                        Log.e("Firestore", "Error loading categories", e);
+                        Toast.makeText(this, "Error loading categories", Toast.LENGTH_SHORT).show();
+                    });
+        }
+    }
+
+    private void loadBudgetsForCategories(List<Category> categories) {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String userId = user.getUid();
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            String[] monthsArray = getResources().getStringArray(R.array.months);
+            String currentMonth = monthsArray[currentMonthIndex];
+            String currentYearStr = String.valueOf(currentYear);
+
+            totalBudget=0.0;
+            for (Category category : categories) {
+                db.collection("users").document(userId).collection("categories")
+                        .document(category.getId()).collection("budgets")
+                        .document(currentYearStr + "_" + currentMonth)
+                        .get()
+                        .addOnSuccessListener(budgetDocument -> {
+                            if (budgetDocument.exists()) {
+                                Double budget = budgetDocument.getDouble("budget");
+                                if (budget != null) {
+                                    totalBudget += budget;
+                                }
+                            }
+                            checkLoadingComplete();
+                            updateRatioUI();
+
+                        })
+                        .addOnFailureListener(e -> {
+                            Log.e("Firestore", "Error loading budget", e);
+                        });
+            }
+        }
+    }
+
+    //verificam daca au fost calculate ambele valori
+    private void checkLoadingComplete() {
+        loadingCounter--;
+        if (loadingCounter == 0) {
+            loadingBar.setVisibility(View.GONE);
+            updateRatioUI();
+            loadingCounter = 2;
+        }
+    }
+
+    // functie pentru update UI
+    private void updateRatioUI() {
+        TextView textExpensesBudget = findViewById(R.id.textExpensesBudget);
+        if (textExpensesBudget != null) {
+            String formattedRatio = String.format("%.2f/%.2f RON", totalExpenses, totalBudget);
+
+            textExpensesBudget.setText(formattedRatio);
+        }
     }
 
     //meniu
@@ -98,7 +252,6 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-
 
     //refresh meniu
     @Override
